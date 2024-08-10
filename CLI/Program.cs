5eef82d49b9ecc4a22870ec745c24fe8b33d148e
Strackeror <rremilavergne@gmail.com>
2024-08-10 18:57:07 +0200

    Improve commandline

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,12 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#nullable enable
+
 using DarkScript3;
+using SoulsFormats;
+using Spectre.Console.Cli;
 
-var options = new EventCFG.CFGOptions();
-var docs = new InstructionDocs("er-common.emedf.json");
-var eventScripter = new FancyEventScripter(
-    new EventScripter(Environment.GetCommandLineArgs()[1], docs),
-    docs,
-    options
-);
+var app = new CommandApp();
+app.Configure(config =>
+    {
+        config.AddCommand<CompileCommand>("compile");
+        config.AddCommand<DecompileCommand>("decompile");
+        config.AddCommand<PreviewCommand>("preview");
+    });
 
-Console.WriteLine(eventScripter.Unpack());
+app.Run(args);
+
+class CompileCommand : Command<CompileCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "[filePath]")]
+        required public string FilePath { get; init; }
+
+        [CommandArgument(1, "[outputPath]")]
+        public string? OutputPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var options = new EventCFG.CFGOptions();
+        var docs = new InstructionDocs("er-common.emedf.json");
+        var eventScripter = new FancyEventScripter(new EventScripter(settings.OutputPath, docs), docs, options);
+        var emevd = eventScripter.Pack(File.ReadAllText(settings.FilePath), settings.FilePath);
+        emevd.Write(settings.OutputPath);
+        return 0;
+    }
+}
+
+class DecompileCommand : Command<DecompileCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "[filePath]")]
+        required public string FilePath { get; init; }
+
+        [CommandArgument(1, "[outputPath]")]
+        public string? OutputPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var options = new EventCFG.CFGOptions();
+        var docs = new InstructionDocs("er-common.emedf.json");
+        var eventScripter = new FancyEventScripter(new EventScripter(settings.FilePath, docs), docs, options);
+        var decompiled = eventScripter.Unpack();
+        if (settings.OutputPath is not null)
+            File.WriteAllText(settings.OutputPath, decompiled);
+        else
+            Console.Write(decompiled);
+        return 0;
+    }
+}
+
+
+class PreviewCommand : Command<PreviewCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "[filePath]")]
+        required public string FilePath { get; init; }
+
+        [CommandOption("--no-fancy")]
+        public bool NoFancy { get; init; }
+    }
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var options = new EventCFG.CFGOptions();
+        var docs = new InstructionDocs("er-common.emedf.json");
+        var eventScripter = new EventScripter("dummy.emevd.dcx", docs, new EMEVD(EMEVD.Game.Sekiro));
+        var fancyEventScripter = new FancyEventScripter(eventScripter, docs, options);
+        fancyEventScripter.Pack(File.ReadAllText(settings.FilePath), settings.FilePath);
+        if (!settings.NoFancy) {
+            Console.Write(fancyEventScripter.Unpack());
+        } else {
+            Console.Write(eventScripter.Unpack());
+        }
+        return 0;
+    }
+}
